52eebbb679c86f5bc0b6a8ca2d2db3c3158310b4
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-06-02 19:04:54 +0530

    Create code.cpp

---
+++ Rotate a Linked List/code.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public
+    Node* rotate(Node* head, int k)
+    {
+        deque<Node*> dq;
+        Node* temp=head;
+        while(temp)
+        {
+            dq.push_back(temp);
+            temp=temp->next;
+        }
+        while(k>0)
+        {
+            Node* t=dq.front();
+            dq.pop_front();
+            dq.push_back(t);
+            k--;
+        }
+        for(int i=0;i<dq.size()-1;i++)
+        {
+            dq[i]->next=dq[i+1];
+        }
+        dq[dq.size()-1]->next=NULL;
+        return dq[0];
+    }
+};
