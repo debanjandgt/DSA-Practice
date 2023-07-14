4564b3d1fbe6949c0ff34fc32fceee361af7d335
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-07-14 23:52:18 +0530

    Create code.cpp

---
+++ Remove Nth Node From End of List/code.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      ListNode temp=head;
+      ArrayList<ListNode> al=new ArrayList<>();
+      while(temp != null)
+      {
+          al.add(temp);
+          temp=temp.next;
+      }
+
+      if(al.size() == 1 && n == 1)
+      return null;
+      if(n>al.size())
+      return null;
+      int k=n;
+        int l=al.size()-k;
+      al.remove(l);
+      if(al.size() == 1)
+      {
+          al.get(0).next=null;
+          return al.get(0);
+      }
+      for(int i=0;i<al.size()-1;i++)
+      {
+          al.get(i).next=al.get(i+1);
+      }
+      al.get(al.size()-1).next=null;
+      return al.get(0);
+    }
+}
