011b30bed94b6ac602962899369b27d23615cb9e
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-08-15 18:15:47 +0530

    Update code.cpp

--- Number of Ways to Arrive at Destination/code.cpp
+++ Number of Ways to Arrive at Destination/code.cpp
@@ -2,14 +2,13 @@ class Solution {
   public:
     int countPaths(int n, vector<vector<int>>& a) {
    vector<pair<long long, long long>> adj[n];
-        // it -> from to weight
         for(auto it:a){
             adj[it[0]].push_back({it[1], it[2]});
             adj[it[1]].push_back({it[0], it[2]});
         }
         vector<long long> ways(n, 0);
         vector<long long> dist(n, LONG_MAX);
-        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq; // dist node
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
         pq.push({0, 0});
         dist[0] = 0;
         ways[0] = 1;
